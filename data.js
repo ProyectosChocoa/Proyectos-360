629afb4a2061864a7114ca14d94fb54ac337f0a2
ProyectosChocoa <54662567+ProyectosChocoa@users.noreply.github.com>
2019-08-29 10:02:48 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,133 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pasillo-360-01",
+      "name": "Pasillo 360 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.009912336323328219,
+        "pitch": -0.011027876692089933,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2519467529489177,
+          "pitch": 0.6192268067358473,
+          "rotation": 18.84955592153877,
+          "target": "1-pasillo-360-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-pasillo-360-02",
+      "name": "Pasillo 360 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8422540939084033,
+          "pitch": 0.6206942853268025,
+          "rotation": 7.853981633974483,
+          "target": "0-pasillo-360-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-pasillo-360-03",
+      "name": "Pasillo 360 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pasillo-360-04",
+      "name": "Pasillo 360 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
